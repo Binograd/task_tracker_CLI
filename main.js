889f3acbde36ced7a6b0db27ec1c989cfc8fca17
Binograd <86157477+Binograd@users.noreply.github.com>
2025-02-21 12:56:06 +0300

    add getTasks method to read tasks.json

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 'use strict'
 import fs from 'fs/promises'
+
+const __dirname = import.meta.dirname
+getTasks()
+
+async function getTasks () {
+  let tasks = {}
+
+  try {
+    const json = await fs.readFile(`${__dirname}/tasks.json`)
+    tasks = await JSON.parse(json)
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      fs.writeFile(`${__dirname}/tasks.json`, '{}')
+    }
+  }
+
+  return tasks
+}
